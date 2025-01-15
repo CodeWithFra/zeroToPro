5057c4c0d3a68baa71114c2d9395ff63a03bac90
Miguel <chri.bellini@gmail.com>
2025-01-16 00:40:22 +0100

    test promise

--- index5.js
+++ index5.js
@@ -1,2 +1,10 @@
-console.log('hey')
-console.log('jhello')
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let success = true; // Cambia a false per testare il rifiuto
+        if (success) {
+            resolve("Operazione completata!"); // Risolve la Promise con successo
+        } else {
+            reject("Errore nell'operazione."); // Rifiuta la Promise in caso di errore
+        }
+    }, 1000);
+});
